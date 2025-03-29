86cc0509a0f838921586adad212408e8a0dafd35
pratheeeksha <prathzz04@gmail.com>
2025-03-29 23:22:02 +0530

    first

--- database.js
+++ database.js
@@ -1,7 +1,7 @@
 // require("dotenv").config();
 // // const { Pool } = require("pg");
 // // const isProduction=process.env.NODE_ENV==="production";
-// // const connectionstring="postgres://yglpwsih:CWRcHriU-lfjazbDAfnD0hvf3f2UZbWz@isilo.db.elephantsql.com/yglpwsih"
+// // const connectionstring=""
 // // const pool=new Pool({
 // //     connectionString: isProduction?process.env.DATABASE_URL:connectionstring
 // // });
@@ -11,7 +11,7 @@
 // //or native libpq bindings
 // //var pg = require('pg').native
 
-// var conString = "postgres://yglpwsih:CWRcHriU-lfjazbDAfnD0hvf3f2UZbWz@isilo.db.elephantsql.com/yglpwsih" //Can be found in the Details page
+// var conString = "" //Can be found in the Details page
 // var client = new pg.Client(conString);
 // client.connect(function(err) {
 //   if(err) {
@@ -30,14 +30,6 @@
 
 
 
-
-
-// const { Pool } = require("pg");
-// const config = "postgres://qfxwijux:sGtZVyXqp9PIG1EaLpZNdZX9qO1RceyE@mouse.db.elephantsql.com/qfxwijux";
-// const pool = new Pool({ connectionString: config, ssl: { rejectUnauthorized: false } });
-// module.exports = pool;
-
-// const { Pool } = require("pg");
 
 
 
